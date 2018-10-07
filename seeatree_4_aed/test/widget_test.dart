e73f9a9c97d4c45274cb426d19f4df2470d77004
Alice <ally.j@live.co.uk>
2018-10-07 21:15:12 +1000

    seaatree_4_aed

--- seeatree_4_aed/test/widget_test.dart
+++ seeatree_4_aed/test/widget_test.dart
@@ -6,24 +6,89 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:seeatree_4_aed/pages/addtree/3_health_page.dart';
+import 'package:seeatree_4_aed/homepage.dart';
+import 'package:seeatree_4_aed/loginpage.dart'; 
+import 'package:seeatree_4_aed/pages/information/rules_page.dart';
+
 
 import 'package:seeatree_4_aed/main.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
+typedef OnObservation = void Function(Route<dynamic> route, Route<dynamic> previousRoute);
 
+class TestObserver extends NavigatorObserver {
+  OnObservation onPushed;
+  OnObservation onPopped;
+  OnObservation onRemoved;
+  OnObservation onReplaced;
+
+  @override
+  void didPush(Route<dynamic> route, Route<dynamic> previousRoute) {
+    if (onPushed != null) {
+      onPushed(route, previousRoute);
+    }
+  }
+
+  @override
+  void didPop(Route<dynamic> route, Route<dynamic> previousRoute) {
+    if (onPopped != null) {
+      onPopped(route, previousRoute);
+    }
+  }
+
+  @override
+  void didRemove(Route<dynamic> route, Route<dynamic> previousRoute) {
+    if (onRemoved != null)
+      onRemoved(route, previousRoute);
+  }
+
+  @override
+  void didReplace({ Route<dynamic> oldRoute, Route<dynamic> newRoute }) {
+    if (onReplaced != null)
+      onReplaced(newRoute, oldRoute);
+  }
+}
+
+
+void main() {
+  testWidgets('Health screen incrementer', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(new MaterialApp(home: new HealthConditionsPage(),));
+  
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('1. Excellent'), findsOneWidget);
+    //expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.widgetWithText(CheckboxListTile, 'A lot of fallen leaves'));
+    await tester.tap(find.widgetWithText(CheckboxListTile, 'Dropped branches with no leaves'));
+    await tester.tap(find.widgetWithText(CheckboxListTile, 'Rotten wood'));
     await tester.pump();
+    //expect(find., findsOneWidget);
+    expect(find.text('2. Good'), findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
+
+  testWidgets('Health screen checkbox', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(new MaterialApp(home: new HealthConditionsPage(),));
+  
+    // Verify that our counter starts at 0.
+    expect(find.text('1. Excellent'), findsOneWidget);
+    //expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.widgetWithText(CheckboxListTile, 'A lot of fallen leaves'));
+    await tester.pump();
+    await tester.tap(find.widgetWithText(CheckboxListTile, 'A lot of fallen leaves'));
+    await tester.pump();
+    //expect(find., findsOneWidget);
+    expect(find.text('1. Excellent'), findsOneWidget);
+
+  });
+
+  
+
+
 }
+
